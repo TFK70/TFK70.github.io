e9aa4bc0f0f32ef33827ad50c854f123e8fce98c
Nikita Elfimov <ne@atls.team>
2021-04-18 01:49:47 +0500

    chore: init

---
+++ react-context/src/store/color/src/index.ts
@@ -0,0 +1,2 @@
+export { Provider as ColorProvider, Context as ColorContext } from './Context'
+export * from './useColor'
---
+++ react-context/src/store/value/src/index.ts
@@ -0,0 +1,2 @@
+export { Provider as ValueProvider, Context as ValueContext } from './Context'
+export * from './useValue'
